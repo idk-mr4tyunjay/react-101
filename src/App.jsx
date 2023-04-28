0bc564a0a23dee8f37c61be59579b47dce863c0d
Mruthunjay <idk.mratyunjay@gmail.com>
2023-04-28 11:00:00 +0530

    image-slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Accordian from './components/accordian'
+import ImageSlider from './components/imageSlider'
 import RandomColor from './components/randomcolor'
 import StarRating from './components/starRating'
 
@@ -11,6 +12,11 @@ function App() {
       <Accordian/>
       <RandomColor/>
       <StarRating noOfStars={10}/>
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   )
 }
